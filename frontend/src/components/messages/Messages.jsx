0d675afbcdd1d65f1716848f63dfe93cdc4f86a9
Ultra-Instinct <134508118+Ultra-Instinc@users.noreply.github.com>
2024-03-29 06:15:08 +0200

    2nd part complete

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Message from "./Message";
+
+export default function Messages() {
+	return (
+		<div className='fle-1 px-4 overflow-auto'>
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+			<Message />
+		</div>
+	);
+}
--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
 
 const SideBar = () => {
 	return (
-		<div>
+		<div className='border-r border-slate-600 p-4 flex flex-col'>
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<Conversations />
-			{/* <LogoutButton /> */}
+			<LogoutButton />
 		</div>
 	);
 };
